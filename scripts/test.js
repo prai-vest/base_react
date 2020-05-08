4df8809527e9f16d629b00daa6f3437109b6494d
Gao <gaotest87@gmail.com>
2020-05-08 12:00:29 -0400

    scripts removed

--- scripts/test.js
+++
@@ -1,8 +0,0 @@
-const parsePage = require('./parsePage')
-
-const args = process.argv.slice(2)
-if (args[0] === 'parsePage') {
-  console.log(`running on ${args[1]}`)
-  const pageToParse = parseInt(args[1], 10) || 1
-  parsePage(pageToParse).then(console.log)
-}
